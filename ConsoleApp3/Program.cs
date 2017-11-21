4fc9f0e0787a19f916fee82ad002f851080c3e5b
Nurik <tanatov1989@mail.ru>
2017-11-21 16:53:47 +0600

    Initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace rara
+{
+class Program
+    {
+        static void Main(string[] args)
+        {   int nul= 0;
+            int[] numbers = new int[]{ 1, 2, 3, 4, 5 };
+            int cosinus = 60; 
+            bool isNumber = false;
+            int randomchislo = 3;
+            Console.Write(Math.Cos(cosinus));
+            Console.BackgroundColor = ConsoleColor.Blue;//sssssssssss comment
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("I like games!!!\n");
+            Console.WriteLine("I like anime!!!");
+            Console.WriteLine(randomchislo);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.Write("Enter the number\n");
+            int nume = int.Parse(Console.ReadLine());
+            Console.Write("your number"nume);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}       
+            
+           
+ 
